70095f9e57c4d1b0fc158523805284a79af67525
SACHIN DEV MANIKANDAN SREE KUMARI <20027623@myrp.edu.sg>
2023-08-08 23:11:38 +0800

    Test cases added headless mode

--- src/test/java/com/rst/helloworld/tests/ScriptTest.java
+++ src/test/java/com/rst/helloworld/tests/ScriptTest.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
@@ -21,7 +22,11 @@ public class ScriptTest {
     @BeforeTest
     public void setup(){
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--no-sandbox");
+
+        driver = new ChromeDriver(options);
         driver.manage().window().maximize();
         driver.get("http://54.255.87.1:8080/simple/#");
 
