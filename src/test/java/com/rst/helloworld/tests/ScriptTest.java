a1c76c0807f7ec1cf19aa1e3ad8102677c0917b9
Fairez-Rashid <106487375+Fairez-Rashid@users.noreply.github.com>
2023-08-11 19:13:34 +0800

    Update ScriptTest.java

--- src/test/java/com/rst/helloworld/tests/ScriptTest.java
+++ src/test/java/com/rst/helloworld/tests/ScriptTest.java
@@ -54,11 +54,17 @@ public class ScriptTest {
 
     }
    @Test
-   public void validateFooterText() throws InterruptedException {
+   public void validateFooterText(String expectedText) throws InterruptedException {
 
-        WebElement footerText = driver.findElement(By.xpath("//h3[contains(text(),'Republic Polytechnic FYP C300 @ Team : SOI-2023-0086')]"));
-        assertTrue(footerText.isDisplayed());
-        System.out.print("Footer text validated \n");
+  WebElement footer = driver.findElement(By.xpath("//footer/p"));
+  
+  String actualText = footer.getText();
+
+  assertTrue(actualText.contains(expectedText));
+
+  System.out.println("Footer text validated");
+
+}
     }
 
 
